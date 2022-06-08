f9c90c1cd4a6f074537edd2ec8e5de00db536878
Alex Kagai <alexkagai@outlook.com>
2022-06-08 09:56:30 +0300

    Add Path and Query params

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, APIRouter
+from typing import Optional
+
+from .data import RECIPES
 
 
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
@@ -12,6 +15,36 @@ def root() -> dict:
     Root GET
     """
     return {"msg": "Hello, World!"}
+
+
+# https://fastapi.tiangolo.com/tutorial/path-params/
+@api_router.get("/recipe/{recipe_id}", status_code=200)
+def fetch_recipe(*, recipe_id: int) -> dict:
+    """
+    Fetch a single recipe by ID
+    """
+
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if result:
+        return result[0]
+
+
+# https://fastapi.tiangolo.com/tutorial/query-params/
+# /search/?keyword=chicken&max_results=2
+@api_router.get("/search/", status_code=200)
+def search_recipes( keyword: Optional[str] = None, 
+                        max_results: Optional[int] = 10) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    if not keyword:
+        # we use Python list slicing to limit results
+        # based on the max_results query parameter
+        return {"results": RECIPES[:max_results]}
+
+    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
+    return {"results": list(results)[:max_results]}
+
 
 
 app.include_router(api_router)
