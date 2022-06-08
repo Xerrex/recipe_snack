0bfb82ae395ff0c0d74e22f7b4700947cea184ae
Alex Kagai <alexkagai@outlook.com>
2022-06-08 12:00:59 +0300

    Add Schemas and Http exceptions

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Query, HTTPException
 from typing import Optional
 
-from .data import RECIPES
+from app.data import RECIPES
+from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
 
 
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
@@ -18,21 +19,26 @@ def root() -> dict:
 
 
 # https://fastapi.tiangolo.com/tutorial/path-params/
-@api_router.get("/recipe/{recipe_id}", status_code=200)
+@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
 def fetch_recipe(*, recipe_id: int) -> dict:
     """
     Fetch a single recipe by ID
     """
 
     result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
-    if result:
-        return result[0]
+    if not result:
+        # the exception is raised, not returned - you will get a validation
+        # error otherwise.
+        raise HTTPException( status_code=404,  
+                                detail=f"Recipe with ID {recipe_id} not found")
+
+    return result[0]
 
 
 # https://fastapi.tiangolo.com/tutorial/query-params/
 # /search/?keyword=chicken&max_results=2
-@api_router.get("/search/", status_code=200)
-def search_recipes( keyword: Optional[str] = None, 
+@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
+def search_recipes( *, keyword: Optional[str] = Query(None, min_length=3, example="chicken"), 
                         max_results: Optional[int] = 10) -> dict:
     """
     Search for recipes based on label keyword
@@ -45,6 +51,24 @@ def search_recipes( keyword: Optional[str] = None,
     results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
     return {"results": list(results)[:max_results]}
 
+
+# New addition, using Pydantic model `RecipeCreate` to define
+# the POST request body
+@api_router.post("/recipe/", status_code=201, response_model=Recipe)
+def create_recipe(*, recipe_in: RecipeCreate) -> dict:
+    """
+    Create a new recipe (in memory only)
+    """
+    new_entry_id = len(RECIPES) + 1
+    recipe_entry = Recipe(
+        id=new_entry_id,
+        label=recipe_in.label,
+        source=recipe_in.source,
+        url=recipe_in.url,
+    )
+    RECIPES.append(recipe_entry.dict())
+
+    return recipe_entry
 
 
 app.include_router(api_router)
